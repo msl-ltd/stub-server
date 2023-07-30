ff8685aa49a5d2a95a974f9e795499539c81037d
msl-ltd <msl-ltd@outlook.jp>
2023-07-30 17:54:00 +0900

    optionsメソッド名を修正する

--- index.js
+++ index.js
@@ -2,39 +2,39 @@ const http = require('http');
 const fs = require('fs');
 
 function getCommandLineValue(argv, argKey) {
-    let argValue;    
+  let argValue;
 
-    for (const arg of argv) {
-        const [key, value] = arg.split('=');
-        if (key === argKey) {
-            argValue = value;
-            break;
-        }
+  for (const arg of argv) {
+    const [key, value] = arg.split('=');
+    if (key === argKey) {
+      argValue = value;
+      break;
     }
+  }
 
-    return argValue;
+  return argValue;
 }
 
 const port = +(getCommandLineValue(process.argv.slice(2), 'port') ?? 3000);
 const status = +(getCommandLineValue(process.argv.slice(2), 'status') ?? 200);
 
 const server = http.createServer((req, res) => {
-    const header = {
-        'Access-Control-Allow-Origin': req.headers.origin,
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Credentials': true,
-    };
+  const header = {
+    'Access-Control-Allow-Origin': req.headers.origin,
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Credentials': true,
+  };
 
-    let body;
-    const origin = header['Access-Control-Allow-Origin'].replace('http://', '').replace(':', '-');
-    const url = req.url.split('?').shift();
-    const path = `./${origin}/${url}/${req.method}/${status}`;
-    if (fs.existsSync(path)) {
-        body = fs.readFileSync(path);
-    }
+  let body;
+  const origin = header['Access-Control-Allow-Origin'].replace('http://', '').replace(':', '-');
+  const url = req.url.split('?').shift();
+  const path = `./${origin}/${url}/${req.method}/${status}`;
+  if (fs.existsSync(path)) {
+      body = fs.readFileSync(path);
+  }
 
-    res.writeHead(req.method === 'OPTION' ? 200 : status, header);
-    res.end(body);
+  res.writeHead(req.method === 'OPTIONS' ? 200 : status, header);
+  res.end(body);
 });
 
 server.listen(port);
